9c169f9045dd0f15b326587781b6b7669a9a16fe
Yuhei Jotaki <jotaki@cloudpack.jp>
2019-05-16 09:46:11 +0900

    docs: update docs

---
+++ src/components/tools.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="tools">
+    <p><input class="input" type="text" v-model="message"></p>
+    <p>{{message}}</p>
+    <button @click="emitEventTest">Button</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Tools',
+  data() {
+    return {
+      message: "from child message"
+    }
+  },
+  methods: {
+    emitEventTest () {
+      this.$emit('event-test',this.message)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.tools {
+  box-sizing: border-box;
+  background: #eee;
+  width: 40%;
+  min-height: 100vh;
+  padding: 20px;
+}
+</style>
