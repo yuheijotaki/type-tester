193d35cef553901865ddd71bd6e058301cb6b8d5
Yuhei Jotaki <jotaki@cloudpack.jp>
2019-05-16 10:56:16 +0900

    feat: form $emit base function

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <template>
   <div id="app">
     <div class="wrapper">
-      <Tools @event-test="clickAlert"></Tools>
+      <div class="tools">
+        <inputText @change="changeText"></inputText>
+      </div>
       <div class="container">
-        <p>{{message}}</p>
+        <div class="container__block">{{message}}</div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Tools from './components/tools'
+import inputText from './components/input-text'
 import "normalize.css";
 
 export default {
   name: 'App',
   components: {
-    Tools
+    inputText
   },
   data() {
     return {
-      message: "default"
+      message: "私はその人を常に先生と呼んでいた。だからここでもただ先生と書くだけで本名は打ち明けない。これは世間を憚かる遠慮というよりも、その方が私にとって自然だからである。私はその人の記憶を呼び起すごとに、すぐ「先生」といいたくなる。筆を執っても心持は同じ事である。よそよそしい頭文字などはとても使う気にならない。",
     }
   },
   methods: {
-    clickAlert: function(message) {
+    changeText: function (message) {
       this.message = message;
     }
   }
@@ -51,10 +53,20 @@ h1,h2,h3,h4,h5,h6 {
 .wrapper {
   display: flex;
 }
+.tools {
+  box-sizing: border-box;
+  background: #eee;
+  width: 40%;
+  min-height: 100vh;
+  padding: 20px;
+}
 .container {
   box-sizing: border-box;
   width: 60%;
   min-height: 100vh;
   padding: 20px;
+}
+.container__block {
+  // white-space: pre;
 }
 </style>
