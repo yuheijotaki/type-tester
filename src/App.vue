a33c27b2a5b1e5442d59e4c6f5382199d8d6fc80
Yuhei Jotaki <jotaki@cloudpack.jp>
2019-05-16 09:47:23 +0900

    feat: $emit base function

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <div class="wrapper">
+      <Tools @event-test="clickAlert"></Tools>
+      <div class="container">
+        <p>{{message}}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Tools from './components/tools'
+import "normalize.css";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Tools
+  },
+  data() {
+    return {
+      message: "default"
+    }
+  },
+  methods: {
+    clickAlert: function(message) {
+      this.message = message;
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+html,* {
+  margin: 0;
+  padding: 0;
+}
+h1,h2,h3,h4,h5,h6 {
+  margin: 0;
+  padding: 0;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Helvetica Neue, Helvetica, Arial, 'Hiragino Kaku Gothic ProN', Meiryo, sans-serif;
+  -webkit-text-size-adjust: 100%;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  text-rendering: optimizeLegibility;
+  font-feature-settings : "palt";
+}
+.wrapper {
+  display: flex;
+}
+.container {
+  box-sizing: border-box;
+  width: 60%;
+  min-height: 100vh;
+  padding: 20px;
 }
 </style>
---
+++ src/components/container.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <Tools @changeMsg="setMessage" :msg="welcomeMsg"/>
+  </div>
+</template>
+
+<script>
+import Tools from "@/components/tools.vue";
+
+export default {
+  name: 'Container',
+  data() {
+    return {
+      welcomeMsg: "Hello World"
+    }
+  },
+  methods: {
+    setMessage(msg) {
+      this.welcomeMsg = msg;
+    }
+  },
+  components: {
+    Tools
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
